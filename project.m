cc32ac526c2da71058c9508a174bd497240de0ff
appari <lalith.appari@students.iiit.ac.in>
2018-11-01 14:10:06 +0530

    Modified the milestones,added report

--- project.m
+++ project.m
@@ -11,6 +11,7 @@ h = fspecial('laplacian');
 filteredout = imfilter(rgb2gray(imread('callender.jpg')),h);
 imshow(filteredout);
 
+
 % Sobel
 [~, threshold] = edge(inp_bw, 'sobel');
 BWs = edge(inp_bw,'sobel', threshold*0.5);
@@ -21,25 +22,31 @@ filteredout = rgb2gray(imread('callender.jpg'));
 canny = edge(filteredout,'canny');
 imshow(canny)
 
-% Canny + Laplacian
+% sobel + Laplacian
+h = fspecial('laplacian')';
 a = imfilter(rgb2gray(imread('callender.jpg')),h);
 b = imbinarize(a);
-c = edge(b);
+c = edge(b,'sobel');
 imshow(c)
 
 
-% Hough transform
-a = imfilter(rgb2gray(imread('callender.jpg')),h);
+% Hough transform on laplacian + sobel
+h = fspecial('laplacian');
+a = imfilter(rgb2gray(imread('callender.jpg')),h');
 b = imbinarize(a);
-BW = edge(b,'canny');
-[H,T,R] = hough(BW);
+imshow(b)
+BW = edge(b,'sobel');
+imshow(BW)
+[H,T,R] = hough(BWs);
 imshow(H,[],'XData',T,'YData',R,...
             'InitialMagnification','fit');
 xlabel('\theta'), ylabel('\rho');
 axis on, axis normal, hold on;
 
-P  = houghpeaks(H,44,'threshold',ceil(0.0000001*max(H(:))));
+P  = houghpeaks(H,100,'threshold',ceil(0.0000001*max(H(:))));
 x = T(P(:,2)); y = R(P(:,1));
+y = y(x==-90|x==0);
+x = x(x==-90|x==0);
 plot(x,y,'s','color','white');
 lines = houghlines(BW,T,R,P,'FillGap',1,'MinLength',1);
 figure, imshow(b), hold on
@@ -59,6 +66,46 @@ for k = 1:length(lines)
       xy_long = xy;
    end
 end
+%
+
+% Hough transform on sobel + sobel
+[~, threshold] = edge(inp_bw, 'sobel');
+BWs = edge(inp_bw,'sobel', threshold*0.5);
+imshow(BWs)
+BW = edge(BWs,'sobel');
+imshow(BW)
+[H,T,R] = hough(BW);
+imshow(H,[],'XData',T,'YData',R,...
+            'InitialMagnification','fit');
+xlabel('\theta'), ylabel('\rho');
+axis on, axis normal, hold on;
+
+P  = houghpeaks(H,100,'threshold',ceil(0.0000001*max(H(:))));
+x = T(P(:,2)); y = R(P(:,1));
+y = y(x==-90|x==0);
+x = x(x==-90|x==0);
+plot(x,y,'s','color','white');
+lines = houghlines(BW,T,R,P,'FillGap',1,'MinLength',1);
+figure, imshow(b), hold on
+max_len = 0;
+for k = 1:length(lines)
+   xy = [lines(k).point1; lines(k).point2];
+   plot(xy(:,1),xy(:,2),'LineWidth',2,'Color','green');
+
+   % Plot beginnings and ends of lines
+   plot(xy(1,1),xy(1,2),'x','LineWidth',2,'Color','yellow');
+   plot(xy(2,1),xy(2,2),'x','LineWidth',2,'Color','red');
+
+   % Determine the endpoints of the longest line segment
+   len = norm(lines(k).point1 - lines(k).point2);
+   if ( len > max_len)
+      max_len = len;
+      xy_long = xy;
+   end
+end
+%
+
+
 %
 binarizedout = imbinarize(filteredout);
 labels = bwlabel(binarizedout);
